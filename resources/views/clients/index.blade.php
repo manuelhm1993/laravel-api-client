8f3900f2d0f34dd9f32ebd8fe61a459e06434a00
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-19 22:32:20 -0400

    Vista index creada, al recuperar los clientes, se transforman en arrays, no objetos

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-4xl font-bold">Listado de clientes</h1>
+    <div class="flex flex-col gap-5">
+        <h1 class="text-4xl font-bold">Listado de clientes</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- Cuando se trabaja con una API Rest la data recuperada son arrays no objetos --}}
+                @foreach ($clients as $client)
+                    <tr>
+                        <td>{{ $client['name'] }}</td>
+                        <td>{{ $client['email'] }}</td>
+                        <td>{{ $client['phone'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::redirect('/', '/clients');
 
 Route::controller(ClientController::class)->prefix('clients')->name('clients.')->group(function () {
