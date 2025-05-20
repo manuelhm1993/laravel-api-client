041d6c0b054e1edbaa4088a015495822fa903ec9
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-19 21:45:32 -0400

    Correr la app laravel-api con php artisan serve para tenerla activa y poderla consumir desde este cliente a través del paquete guzzle, también se recupera en json y se recorre con un foreach

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http; // Facade para trabajar con requests http
 
 Route::get('/', function () {
-    return view('welcome');
+    $response = Http::get('http://127.0.0.1:8000/api/clients');
+    $clients = $response->json();
+
+    echo "<ul>";
+    foreach($clients as $client)
+    {
+        echo "<li>{$client['name']} - {$client['email']}</li>";
+    }
+    echo "</ul>";
 });
